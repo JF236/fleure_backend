a633319ee6529a9f9535d988471f3c52a1982d3f
edlee <edlee05072002@gmail.com>
2024-07-06 22:23:43 -0400

    Make sure unit tests for bundle-tag run

--- workers/bundle-tag/jest.config.js
+++ workers/bundle-tag/jest.config.js
@@ -1,15 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.json',
-      },
+      '^.+\\.tsx?$': 'ts-jest',
     },
   };
-  
